71aec0e45942efe0e808f0ad76628ba07f2c4e14
unknown <Administrador@.(none)>
2011-08-04 08:07:24 -0300

    inserindo projeto mais arquivo do proteus

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <16f876a.h>
+#use delay(clock=16000000)
+#include <lcd_modificado.c>
+#include <Funçoes.c>
+void main()
+{
+   char x,i;
+   inicializa_lcd();
+   port_b_pullups(true);
+   while(1)
+   {
+
+   }
+}
