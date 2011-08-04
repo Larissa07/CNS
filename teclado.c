9969ab0527fd0be9b8ead18f54a1fc5524056cf9
Larissa07 <larissa.splex@hotmail.com>
2011-08-04 08:42:04 -0300

    adicionando arquivo para leitura de teclado fora do arquivo de funcoes

---
+++ teclado.c
@@ -0,0 +1,12 @@
+#define baixo    PIN_B4
+#define direita  PIN_B5
+#define esquerda PIN_B2
+#define cima     PIN_B3
+void teclado()
+{
+   char tecla=0;
+   if(!input(cima))tecla=1;
+   if(!input(esquerda))tecla=2;
+   if(!input(direita))tecla=3;
+   if(!input(baixo))tecla=4;
+}
